054535c487f62c8002eba9d64150727db3600349
Erick Lucas <99370640+ErickElc@users.noreply.github.com>
2022-10-15 16:19:43 -0300

    fix

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const multer = require('multer');
 const postController = require('../controllers/postController.js');
 const multerConfig = require('../config/multer');
 
